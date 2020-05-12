d2bc0603b79ab4f91d7aeab98a90213a3bd2718d
popcorn <popcorn9499@gmail.com>
2020-05-12 02:56:23 -0500

    working

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ import asyncio
 from EventHandler import EventHandler
 from InputHandler import InputHandler
 from ProgramHandler import ProgramHandler 
+import KeyPressHandler
 
 from evdev.ecodes import *
 
 device = InputHandler("/dev/input/by-path/platform-i8042-serio-0-event-kbd",grabbed=True)
+e = evdev.ecodes
+uinput = evdev.UInput()
 
-#MAke event handle key and key state
-@device.eventHandler(KEY_LEFTCTRL+KEY_A)
+
+
+@device.eventHandler(KEY_A)
 async def foo(event):
     ProgramHandler("cat /home/popcorn9499/iommuGroups.sh")
+    
+    if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
+        key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_M])
+        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+
 
 @device.eventHandler(KEY_B)
 async def bar(event):
