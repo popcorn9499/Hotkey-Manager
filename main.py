2531b352051d474a7c7c88364f688ec112e4f082
popcorn <popcorn9499@gmail.com>
2022-07-11 00:40:04 -0500

    testing out the obs websocket

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import obsWSHandler
 
 '''
 note to anyone using this software. please dont use modifier keys with it 
-Ctrl+c and ctrl+z will activate other software as macros a single key presses
+Ctrl+c and ctrl+z will activate other software as macros a singlq key presses
 '''
 
 
@@ -36,50 +36,59 @@ obsWS = obsWSHandler.obsWSHandler("WS_Config.json")
 async def micMuteStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_L])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
-
-@device.eventHandler(KEY_1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.toggleMute("Mic/Aux")
+        
+        
+@device.eventHandler(KEY_1)2
 async def gameMuteStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_1])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEYq_TOGGLE,pressDuration=0.1)
+        await obsWS.toggleMute("Desktop Audio")
+        await obsWS.toggleMute("Elgato HD 1080P")
 
 @device.eventHandler(KEY_A)
 async def discordMuteStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_SEMICOLON])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.toggleMute("Communication Audio")
 
 @device.eventHandler(KEY_Z)
 async def musicMuteStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_P])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.toggleMute("Music")
 
 @device.eventHandler(KEY_X)
 async def brbSceneStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_SLASH])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.setCurrentScene("Be Right Back")
 
 @device.eventHandler(KEY_S)
 async def gameSceneStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_J])
-
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.setCurrentScene("Game")
 
 @device.eventHandler(KEY_W)
 async def endingSceneStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_W])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.setCurrentScene("Ending")
 
 @device.eventHandler(KEY_2)
 async def startingSceneStream(event):
     if event.value == KeyPressHandler.KEY_STATE.KEY_DOWN:
         key = KeyPressHandler.KeyPress(keys=[KEY_LEFTCTRL,KEY_2])
-        await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        #await key.keyPress(keyState=KeyPressHandler.KEY_STATE.KEY_TOGGLE,pressDuration=0.1)
+        await obsWS.setCurrentScene("Starting")
 
 @device.eventHandler(KEY_C)
 async def toggleNDICaptureStream(event):
