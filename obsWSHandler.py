a176221d452cb5862dbce5b061418c2ba9e0c982
popcorn <popcorn9499@gmail.com>
2022-07-11 01:23:27 -0500

    added comments

--- obsWSHandler.py
+++ obsWSHandler.py
@@ -4,6 +4,13 @@ import asyncio
 
 
 class obsWSHandler:
+    '''
+    this constructor takes takes
+        self
+        confName string
+    returns
+        none
+    '''
     def __init__(self, confName):
         conf = config.load(confName)
         self._url = conf["obs_ws_url"]
@@ -12,6 +19,7 @@ class obsWSHandler:
 
         self._ws = simpleobsws.WebSocketClient(url = self._url, password = self._password, identification_parameters = parameters) # Every possible argument has been passed, but none are required. See lib code for defaults.
 
+    #function to be called when starting the program
     async def init(self):
         await self.connect()
         request = simpleobsws.Request('GetVersion') # Build a Request object
@@ -26,18 +34,45 @@ class obsWSHandler:
             await asyncio.sleep(30)
         await ws.disconnect() # Disconnect from the websocket server cleanly
 
+    '''
+    this function takes
+        self
+        name string
+    returns
+        none
+    '''
     async def toggleMute(self,name):
         request = simpleobsws.Request("ToggleInputMute", requestData={"inputName": name})
         ret = await self._call(request)
 
+    '''
+    this function takes
+        self
+        name string
+    returns
+        none
+    '''
     async def setCurrentScene(self,name):
         request = simpleobsws.Request("SetCurrentProgramScene", requestData={"sceneName": name})
         ret = await self._call(request)
 
+    '''
+    this function takes
+        self
+        name string
+    returns
+        none
+    '''
     async def saveReplayBuffer(self):
         request = simpleobsws.Request("SaveReplayBuffer")
         ret = self._call(request)
-
+    '''
+    this function takes
+        self
+        request simpleobsws.Request
+    returns
+        none
+    '''
     async def _call(self, request):
         try:
             await self._ws.call(request)
